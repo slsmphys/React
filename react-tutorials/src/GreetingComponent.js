9e100f45e0587552017ab945065b061774717f03
slsmphys@gmail.com <slsmphys@gmail.com>
2018-05-27 07:55:02 +0100

    Components 2 - Stateful declaration of a component

---
+++ react-tutorials/src/GreetingComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class GreetingComponent extends React.Component {
+  render() {
+    return (
+      <h1>Hello There!</h1>
+    );
+  }
+}
+
+export default GreetingComponent
